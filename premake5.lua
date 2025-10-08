31a8f9f53e74530d6ec3c2fe1ba30a58aac9cfe0
rohan37kumar <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>
2025-10-09 01:24:03 +0530

    create: Premake file

---
+++ premake5.lua
@@ -0,0 +1,111 @@
+workspace "musashi"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "musashi"
+	location "musashi"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/external/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		buildoptions { "/utf-8" }
+
+		defines
+		{
+			"MSSHI_PLATFORM_WINDOWS",
+			"MSSHI_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "MSSHI_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "MSSHI_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "MSSHI_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"musashi/external/spdlog/include",
+		"musashi/src"
+	}
+
+	links
+	{
+		"musashi"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		buildoptions { "/utf-8" }
+
+		defines
+		{
+			"MSSHI_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "MSSHI_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "MSSHI_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "MSSHI_DIST"
+		optimize "On"
+
